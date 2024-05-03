81c73108b3dd10b932df97b92d52f908b82d6438
YataYT <151573037+YataYT@users.noreply.github.com>
2024-05-03 10:03:01 -0500

    MutantGuardian refactor

--- Content/Bosses/MutantBoss/MutantProjectiles/MutantGuardian.cs
+++ Content/Bosses/MutantBoss/MutantProjectiles/MutantGuardian.cs
@@ -45,6 +45,18 @@ namespace FargowiltasSouls.Content.Bosses.MutantBoss
             return target.hurtCooldowns[1] == 0;
         }
 
+        public override void OnSpawn()
+        {
+            Projectile.rotation = Main.rand.NextFloat(0, MathHelper.TwoPi);
+            Projectile.hide = false;
+
+            for (int i = 0; i < 5; i++)
+            {
+                int d = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Blood, Alpha: 100, Scale: 2f);
+                Main.dust[d].noGravity = true;
+            }
+        }
+
         public ref float AI0 => ref Projectile.ai[0];
         public ref float AI1 => ref Projectile.ai[1];
         public ref float AI2 => ref Projectile.ai[2];
@@ -54,34 +66,20 @@ namespace FargowiltasSouls.Content.Bosses.MutantBoss
 
         public override void AI()
         {
-            if (Projectile.localAI[0] == 0)
-            {
-                Projectile.localAI[0] = 1;
-                Projectile.rotation = Main.rand.NextFloat(0, 2 * (float)Math.PI);
-                Projectile.hide = false;
-
-                for (int i = 0; i < 30; i++)
-                {
-                    int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Blood, 0, 0, 100, default, 2f);
-                    Main.dust[dust].noGravity = true;
-                }
-            }
-
             Projectile.frame = 2;
             Projectile.direction = Projectile.velocity.X < 0 ? -1 : 1;
-            Projectile.rotation += Projectile.direction * .3f;
+            Projectile.rotation += Projectile.direction * 0.3f;
         }
 
         public override void OnHitPlayer(Player target, Player.HurtInfo info)
         {
+            // Lots of debuffs
             if (WorldSavingSystem.EternityMode)
             {
                 target.FargoSouls().MaxLifeReduction += 100;
                 target.AddBuff(ModContent.BuffType<OceanicMaulBuff>(), 5400);
                 if (WorldSavingSystem.MasochistModeReal)
-                {
                     target.AddBuff(ModContent.BuffType<GodEaterBuff>(), 420);
-                }
                 target.AddBuff(ModContent.BuffType<FlamesoftheUniverseBuff>(), 420);
                 target.AddBuff(ModContent.BuffType<MarkedforDeathBuff>(), 420);
                 target.AddBuff(ModContent.BuffType<MutantFangBuff>(), 180);
@@ -91,7 +89,8 @@ namespace FargowiltasSouls.Content.Bosses.MutantBoss
 
         public override void OnKill(int timeLeft)
         {
-            for (int i = 0; i < 30; i++)
+            // Dust!!!
+            for (int i = 0; i < 5; i++)
             {
                 int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Blood, 0, 0, 100, default, 2f);
                 Main.dust[dust].noGravity = true;
@@ -100,27 +99,8 @@ namespace FargowiltasSouls.Content.Bosses.MutantBoss
 
         public override bool PreDraw(ref Color lightColor)
         {
-            Texture2D texture2D13 = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value;
-            int num156 = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value.Height / Main.projFrames[Projectile.type]; //ypos of lower right corner of sprite to draw
-            int y3 = num156 * Projectile.frame; //ypos of upper left corner of sprite to draw
-            Rectangle rectangle = new(0, y3, texture2D13.Width, num156);
-            Vector2 origin2 = rectangle.Size() / 2f;
+            Utilities.GetAfterimagesCentered(Projectile, smth);
 
-            Color color26 = lightColor;
-            color26 = Projectile.GetAlpha(color26);
-
-            SpriteEffects effects = SpriteEffects.None;
-
-            for (int i = 0; i < ProjectileID.Sets.TrailCacheLength[Projectile.type]; i++)
-            {
-                Color color27 = color26 * 0.5f;
-                color27 *= (float)(ProjectileID.Sets.TrailCacheLength[Projectile.type] - i) / ProjectileID.Sets.TrailCacheLength[Projectile.type];
-                Vector2 value4 = Projectile.oldPos[i];
-                float num165 = Projectile.oldRot[i];
-                Main.EntitySpriteDraw(texture2D13, value4 + Projectile.Size / 2f - Main.screenPosition + new Vector2(0, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), color27, num165, origin2, Projectile.scale, effects, 0);
-            }
-
-            Main.EntitySpriteDraw(texture2D13, Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), color26, Projectile.rotation, origin2, Projectile.scale, effects, 0);
             return false;
         }
     }
